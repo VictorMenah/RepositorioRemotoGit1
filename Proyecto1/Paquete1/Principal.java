8238850d723089692abc9d6e01f28ac137bbc41e
DAW <DAW@DESKTOP-U4FTC0L>
2024-01-16 13:12:58 +0100

    segundo ejemplo

---
+++ Proyecto1/Paquete1/Principal.java
@@ -0,0 +1,8 @@
+package Paquete1;
+
+public class Principal {
+	public static void main(String[] args) {
+	System.out.println("Ejemplo control de versiones");
+	System.out.println("Segundo ejemplo de control de versiones con Git");
+	}
+}
